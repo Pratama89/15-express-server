b2d94b42b2e056e0766b7d53a49c1037705378e8
Pratama89 <pratama.adiciptawan@gmail.com>
2023-05-24 21:16:36 +0700

    menambah file baru

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const expressLayouts = require('express-ejs-layouts')
-
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
 // Gunakan EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+// Third-party Middleware
 app.use(expressLayouts);
+app.use(morgan("dev"));
+
+// Built-in Middleware
+app.use(express.static("public"));
+
+// Application Middleware
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -16,22 +28,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.render('about', {
-    layout: 'layouts/main-layout',
-    title: 'Halaman About'
+  res.render("about", {
+    layout: "layouts/main-layout",
+    title: "Halaman About",
   });
 });
 
 app.get("/contact", (req, res) => {
-  res.render('contact', {
-    layout: 'layouts/main-layout',
-    title: 'Halaman Contact',
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Halaman Contact",
   });
 });
 
 app.use("/", (req, res) => {
-  res.status(404)
-  res.send("404");
+  res.status(404);
+  res.render("error404", {
+    layout: "layouts/main-layout",
+    title: "Error 404",
+  });
 });
 
 
