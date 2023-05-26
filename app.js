99b4bbb0b99451834c9b21976eaaebb6e6d1a8c5
Pratama89 <pratama.adiciptawan@gmail.com>
2023-05-26 08:35:39 +0700

    menambahkan informasi detail

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const expressLayouts = require('express-ejs-layouts')
-const { loadContact } = require("./utils/contacts");
+const { loadContact, findContact } = require("./utils/contacts");
 
 const app = express();
 const port = 3000;
